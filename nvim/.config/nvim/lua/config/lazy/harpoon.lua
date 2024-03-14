03fbbd7641439a51f9b5e83fc0b31c69f4f3971b
jpedro85 <pedro.games30@gmail.com>
2024-03-14 09:59:23 +0000

    fix: removed temporarly the binds in harpoon

--- nvim/.config/nvim/lua/config/lazy/harpoon.lua
+++ nvim/.config/nvim/lua/config/lazy/harpoon.lua
@@ -11,9 +11,9 @@ return {
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         -- TODO: Change binding to something that make more sense
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+        -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+        -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+        -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+        -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
     end
 }
