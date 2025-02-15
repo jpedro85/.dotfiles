de3ef21a51cb70fb561ba41288dd28575b663a78
jpedro85 <pedro.games30@gmail.com>
2025-02-15 17:54:15 +0000

    Improved Screenshot script and cursor theme in hyprland

--- hyperland/.config/hypr/scripts/screenshot.sh
+++ hyperland/.config/hypr/scripts/screenshot.sh
@@ -1,98 +1,80 @@
 #!/usr/bin/env zsh
 
+OUTPUT_FOLDER="$HOME/Pictures/screenshots"
+
 # Function to create directory if not exist
 create_screenshot_dir() {
-    mkdir -p "$HOME/Pictures/screenshots"
+    mkdir -p OUTPUT_FOLDER
 }
-check_dunst_installed() {
-    if ! command -v dunst &>/dev/null; then
-        notify-send "dunst could not be found. Please install dunst."
-        echo "dunst could not be found. Please install dunst."
+
+# Function to check if a command is installed
+check_command() {
+    if ! command -v "$1" &>/dev/null; then
+        notify-send "$1 could not be found. Please install $1."
+        echo "$1 could not be found. Please install $1."
         exit 1
     fi
 }
 
-# Function to check if grim is installed
-check_grim_installed() {
-    if ! command -v grim &>/dev/null; then
-        notify-send "grim could not be found. Please install grim."
-        echo "grim could not be found. Please install grim."
-        exit 1
-    fi
-}
+# Check all required dependencies
+dependencies=("dunst" "grim" "slurp" "wl-copy" "rofi" "hyprshot" "ksnip")
+for cmd in "${dependencies[@]}"; do
+    check_command "$cmd"
+done
 
-# Function to check if slurp is installed
-check_slurp_installed() {
-    if ! command -v slurp &>/dev/null; then
-        notify-send "slurp could not be found. Please install slurp."
-        echo "slurp could not be found. Please install slurp."
-        exit 1
-    fi
-}
-
-# Function to check if wl-copy is installed
-check_wl_copy_installed() {
-    if ! command -v wl-copy &>/dev/null; then
-        notify-send "wl-copy could not be found. Please install wl-copy."
-        echo "wl-copy could not be found. Please install wl-copy."
-        exit 1
-    fi
-}
-
-# Function to check if dmenu is installed
-check_rofi_installed() {
-    if ! command -v rofi &>/dev/null; then
-        notify-send "rofi could not be found. Please install rofi-wayland."
-        echo "rofi could not be found. Please install rofi-wayland."
-        exit 1
-    fi
-}
-
-# Function to take a screenshot and save it to a file
+# Function to take a screenshot to file using Hyprshot
 take_screenshot_to_file() {
-    currentDate=$(date '+%Y_%m_%d')
-    fileName="$HOME/Pictures/screenshots/screenshot_${RANDOM}_$currentDate.png"
-
-    if flameshot gui -p "$fileName"; then
-        wl-copy <"$fileName" && notify-send "Screenshot" "Saved to $fileName and copied to clipboard."
+    local current_date=$(date '+%Y_%m_%d')
+    local fileName="$HOME/Pictures/screenshots/screenshot_${RANDOM}_${current_date}.png"
+    if hyprshot -m region -o "$fileName"; then
+        wl-copy < "$fileName"
+        notify-send "Screenshot" "Saved to $fileName and copied to clipboard."
     else
-        notify-send "Error" "Failed to take screenshot or copy to clipboard."
+        notify-send "Error" "Failed to take screenshot."
+        echo "Error: Failed to take screenshot."
     fi
 }
 
-# Function to take a screenshot and copy it directly to the clipboard
+# Function to take a screenshot to clipboard using Hyprshot
 take_screenshot_to_clipboard() {
-    if flameshot gui -r | wl-copy; then
+    if hyprshot -r -z -m region  | wl-copy; then
         notify-send "Screenshot" "Copied to clipboard."
-        echo "Screenshot" "Copied to clipboard."
     else
-        notify-send "Error" "Failed to take screenshot or copy to clipboard."
-        echo "Error" "Failed to take screenshot or copy to clipboard."
+        notify-send "Error" "Failed to take screenshot."
+        echo "Error: Failed to take screenshot."
     fi
 }
 
-# Main script execution
+# Function to take a screenshot and annotate with Knsip before copying to clipboard
+annotate_screenshot_to_clipboard() {
+    local current_date=$(date '+%Y_%m_%d')
+    local fileName="screenshot_${RANDOM}_${current_date}.png"
+    local filePath="$OUTPUT_FOLDER/$fileName"
 
-check_dunst_installed
-check_grim_installed
-check_slurp_installed
-check_wl_copy_installed
-check_rofi_installed
+    hyprshot -m region -o $OUTPUT_FOLDER -f $fileName
+    ksnip $filePath && wl-copy < "$filePath"
+}
+
+# Create screenshot directory
 create_screenshot_dir
 
-choice=$(echo -e "Save to file and clipboard\nCopy to clipboard only" | rofi -dmenu -p "Choose screenshot action")
+choice=$(echo -e "Save to file and clipboard\nCopy to clipboard only\nAnnotate screenshot (saves to a file)" | rofi -dmenu -p "Choose screenshot action")
+
 # To ensure rofi is out of the screenshot
 sleep 0.29
 
 case $choice in
-"Save to file and clipboard")
-    take_screenshot_to_file
-    ;;
-"Copy to clipboard only")
-    take_screenshot_to_clipboard
-    ;;
-*)
-    notify-send "Screenshot" "Invalid choice. Exiting."
-    echo "Screenshot" "Invalid choice. Exiting."
-    ;;
+
+    "Save to file and clipboard")
+        take_screenshot_to_file
+        ;;
+    "Copy to clipboard only")
+        take_screenshot_to_clipboard
+        ;;
+    "Annotate screenshot (saves to a file)")
+        annotate_screenshot_to_clipboard
+        ;;
+    *)
+        notify-send "Screenshot" "Invalid choice. Exiting."
+        ;;
 esac
