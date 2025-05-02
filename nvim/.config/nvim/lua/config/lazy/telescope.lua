f1486dd873eb7bf645ab51819425b385733c51f1
jpedro85 <104324234+jpedro85@users.noreply.github.com>
2025-05-02 23:40:49 +0100

    updated nvim plugins, disabled preview telescope

--- nvim/.config/nvim/lua/config/lazy/telescope.lua
+++ nvim/.config/nvim/lua/config/lazy/telescope.lua
@@ -1,22 +1,25 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    tag = "0.1.8",
     dependencies = {
         "nvim-lua/plenary.nvim"
     },
     config = function()
         require("telescope").setup({
             defaults = {
+                preview = false,
                 file_ignore_patterns = {
-                "node_modules","docs"
+                    "node_modules",
+                    "docs",
+                    "build"
                 }
             }
         })
 
         local builtin = require("telescope.builtin")
         vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Search Files in Dir" })
-        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {desc = "Search string in the project"})
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = "Search string in the project" })
         vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Search Files in Repo" })
-        vim.keymap.set('n', 'gr', builtin.lsp_references, {desc = "Search References"})
+        vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = "Search References" })
     end
 }
