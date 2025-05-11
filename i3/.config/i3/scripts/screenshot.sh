76c06001d917b474c0ccd285fd60f986844cb659
jpedro85 <104324234+jpedro85@users.noreply.github.com>
2025-05-11 18:27:03 +0100

    refactor: Passed the envsetup and clean-env to bash
    - Added the feature to check for packages for a certain environment
    - Added the .meta dir for packages information

--- i3/.config/i3/scripts/screenshot.sh
+++ i3/.config/i3/scripts/screenshot.sh
@@ -1,37 +1,37 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 
 # Function to create directory if not exist
 create_screenshot_dir() {
-  mkdir -p "$HOME/Pictures/screenshots"
+    mkdir -p "$HOME/Pictures/screenshots"
 }
 
 # Function to check if rofi is installed
 check_rofi_installed() {
-  if ! command -v rofi &> /dev/null; then
-    echo "rofi could not be found. Please install rofi."
-    exit 1
-  fi
+    if ! command -v rofi &>/dev/null; then
+        echo "rofi could not be found. Please install rofi."
+        exit 1
+    fi
 }
 
 # Function to take a screenshot and save it to a file
 take_screenshot_to_file() {
-  currentDate=$(date '+%Y_%m_%d')
-  fileName="$HOME/Pictures/screenshots/screenshot_${RANDOM}_$currentDate.png"
+    currentDate=$(date '+%Y_%m_%d')
+    fileName="$HOME/Pictures/screenshots/screenshot_${RANDOM}_$currentDate.png"
 
-  if flameshot gui -p "$fileName"; then
-    xclip -selection clipboard -t image/png -i "$fileName" && notify-send "Screenshot" "Saved to $fileName and copied to clipboard."
-  else
-    notify-send "Error" "Failed to take screenshot or copy to clipboard."
-  fi
+    if flameshot gui -p "$fileName"; then
+        xclip -selection clipboard -t image/png -i "$fileName" && notify-send "Screenshot" "Saved to $fileName and copied to clipboard."
+    else
+        notify-send "Error" "Failed to take screenshot or copy to clipboard."
+    fi
 }
 
 # Function to take a screenshot and copy it directly to the clipboard
 take_screenshot_to_clipboard() {
-  if flameshot gui -c; then
-    notify-send "Screenshot" "Copied to clipboard."
-  else
-    notify-send "Error" "Failed to take screenshot or copy to clipboard."
-  fi
+    if flameshot gui -c; then
+        notify-send "Screenshot" "Copied to clipboard."
+    else
+        notify-send "Error" "Failed to take screenshot or copy to clipboard."
+    fi
 }
 
 # Main script execution
@@ -42,13 +42,13 @@ create_screenshot_dir
 choice=$(echo -e "Save to file and clipboard\nCopy to clipboard only" | rofi -dmenu -p "Choose screenshot action")
 
 case $choice in
-  "Save to file and clipboard")
+"Save to file and clipboard")
     take_screenshot_to_file
     ;;
-  "Copy to clipboard only")
+"Copy to clipboard only")
     take_screenshot_to_clipboard
     ;;
-  *)
+*)
     notify-send "Screenshot" "Invalid choice. Exiting."
     ;;
 esac
--- nvim/.config/nvim/lua/config/lazy/lsp.lua
+++ nvim/.config/nvim/lua/config/lazy/lsp.lua
@@ -1,17 +1,17 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim", version = "^1.0.0" },
+    { "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" },
+    { "L3MON4D3/LuaSnip" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "j-hui/fidget.nvim" },
+  },
 
     config = function()
         local cmp = require('cmp')
@@ -27,9 +27,7 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
                 "clangd",
-                "ts_ls",
                 "jdtls",
             },
             handlers = {
